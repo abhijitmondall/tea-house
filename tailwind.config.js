2b52904d3bc667ee4cd383f6e9f6a1146018ba3a
abhijitmondall <abhijitmondal.cse@gmail.com>
2023-01-22 03:09:05 +0600

    Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryColor: "#FF0000",
+        secondaryColor: "#FF8938",
+        grayColor: "#F4F4F4",
+        darkColor: "#1C1C1C",
+        yellowColor: "#FFC850",
+      },
+    },
   },
   plugins: [],
 };
